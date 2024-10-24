aaaebb2565d1e1d39f193a4fbffff17b31ee2470
Анна <vindanka@gmail.com>
2024-10-24 23:06:12 +0300

    create 3 tasks, wait for ending

--- Task9/Program.cs
+++ Task9/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Task task1 = Task.Run(() =>
+{
+    Console.WriteLine("Hello from task1");
+});
+
+Task task2 = Task.Run(() =>
+{
+    Console.WriteLine(2924 + 2354);
+});
+
+Task task3 = Task.Run(async () =>
+{
+    Console.WriteLine("10s");
+    await Task.Delay(3000);
+});
+
+Task.WaitAll(task1, task2, task3);
+Console.WriteLine("after waiting");
